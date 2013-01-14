e605ce44d253edfaba52ff7e63f39d52517c2ca7
Alexandre Blin <alex@madjawa.net>
2013-01-14 16:30:09 +0100

    Added basic application

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, url_for, g
+app = Flask(__name__)
+
+from google.appengine.ext import db
+from google.appengine.api import users
+
+@app.route('/')
+def index():
+	return render_template('index.html')
