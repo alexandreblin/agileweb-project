3d8fddd7a6338cc2ca90b834dd98e86ec555805c
Alexandre Blin <alex@madjawa.net>
2013-01-21 16:38:45 +0100

    'New game' button

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, render_template, redirect, url_for, g
+from flask import Flask, request, render_template, redirect, url_for
 from model.dictionary import Dictionary
 from model.game import Game
+import binascii
+import os
 
 app = Flask(__name__)
 
-from google.appengine.ext import db
-from google.appengine.api import users
+games = {}
 
 game = Game()
    
@@ -28,6 +29,7 @@ def test_word():
 
     return render_template('test_word.html', word=word, valid=isvalid)
 
+
 @app.route('/word')
 def word():
     word = None
@@ -36,3 +38,23 @@ def word():
     game.printGameField()
 
     return render_template('index.html')
+
+
+@app.route('/new')
+def newgame():
+    # generate an ID that is not already used
+    uniqueID = binascii.hexlify(os.urandom(8))
+    while uniqueID in games:
+        uniqueID = binascii.hexlify(os.urandom(8))
+
+    games[uniqueID] = {}
+
+    return redirect(url_for('game', gameId=uniqueID))
+
+
+@app.route('/game/<gameId>')
+def game(gameId):
+    if gameId not in games:
+        return 'Invalid game ID'
+
+    return render_template('game.html')
