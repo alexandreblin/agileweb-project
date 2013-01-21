092cf684e381d3186d7653808c3858ebfa090ed0
Alexandre Blin <alex@madjawa.net>
2013-01-21 15:10:49 +0100

    Test page for dictionary

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 from google.appengine.ext import db
 from google.appengine.api import users
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/test_word', methods=['GET', 'POST'])
+def test_word():
+    word = None
+    isvalid = None
+
+    if request.method == 'POST':
+        dictionary = Dictionary('dictionary.txt')
+        word = request.form['word']
+        isvalid = dictionary.isWordValid(word)
+
+    return render_template('test_word.html', word=word, valid=isvalid)
