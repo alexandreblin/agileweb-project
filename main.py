d8289593b2b715e5159ab61971e256f9d2f6628c
Alexandre Blin <alex@madjawa.net>
2013-01-29 11:13:03 +0100

    Reverted last push

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, g
 from model.dictionary import Dictionary
 from model.game import Game
-import binascii
-import os
 
 app = Flask(__name__)
 
-games = {}
+from google.appengine.ext import db
+from google.appengine.api import users
 
 game = Game()
    
@@ -29,7 +28,6 @@ def test_word():
 
     return render_template('test_word.html', word=word, valid=isvalid)
 
-
 @app.route('/word')
 def word():
     word = None
@@ -38,23 +36,3 @@ def word():
     game.printGameField()
 
     return render_template('index.html')
-
-
-@app.route('/new')
-def newgame():
-    # generate an ID that is not already used
-    uniqueID = binascii.hexlify(os.urandom(8))
-    while uniqueID in games:
-        uniqueID = binascii.hexlify(os.urandom(8))
-
-    games[uniqueID] = {}
-
-    return redirect(url_for('game', gameId=uniqueID))
-
-
-@app.route('/game/<gameId>')
-def game(gameId):
-    if gameId not in games:
-        return 'Invalid game ID'
-
-    return render_template('game.html')
